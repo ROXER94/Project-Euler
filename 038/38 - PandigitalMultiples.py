867afe9afe7bbac05849fdf25f5e0a76158d9b04
Robert Oxer <rdo26@cornell.edu>
2016-08-09 17:23:17 -0400

    Add files via upload

---
+++ 038/38 - PandigitalMultiples.py
@@ -0,0 +1,19 @@
+# Calculates the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2,... ,n) where n > 1
+
+def pandigital(n):
+	total = ""
+	i = 1
+	while len(total) < 10:
+		total += str(n * i)
+		i += 1
+	total = total[:9]
+	if len("".join(set(total))) == 9 and "0" not in "".join(set(total)):
+	    return int(total)
+	return 0
+
+max = 0
+for i in range(9999,9000,-1):
+	current = pandigital(i)
+	if current > max:
+		max = current
+print(max)
---
+++ 038/README.md
@@ -0,0 +1,5 @@
+# PandigitalMultiples
+
+From [Project Euler: Problem 38](https://projecteuler.net/problem=38)
+
+Calculates the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2,... ,n) where n > 1
