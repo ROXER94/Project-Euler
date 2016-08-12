445de7b9c9520642eff20c6b9ee42bc59d33b58e
Robert Oxer <rdo26@cornell.edu>
2016-08-12 15:37:22 -0400

    Add files via upload

--- 010/10 - SummationOfPrimes.py
+++ 010/10 - SummationOfPrimes.py
@@ -1,10 +1,6 @@
 # Calculates the sum of all primes below 2,000,000
 
 def isPrime(n):
-    if n < 2:
-        return False
-    if n == 2: 
-        return True    
     if not n & 1: 
         return False
     for x in range(3, int(n**0.5) + 1, 2):
