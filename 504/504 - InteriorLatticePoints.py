2c924f0166d868962163a4c1e50ab0012405ac72
Robert Oxer <rdo26@cornell.edu>
2016-08-21 22:06:45 -0400

    Add files via upload

--- 504/504 - InteriorLatticePoints.py
+++ 504/504 - InteriorLatticePoints.py
@@ -1,11 +1,13 @@
 # Calculates the number of quadrilaterals that contain a square number of lattice points for a given range of coordinates
 
-Range = 100
+Range = 4
 
 from math import sin, acos, sqrt
 from fractions import gcd
 
 array = []
+cacheArea = {}
+cacheExterior = {}
 
 # Generate the points of the quadrilaterals
 for a in range(1,Range+1):
@@ -26,38 +28,40 @@ def is_square(integer):
 def distance(x1,y1,x2,y2):
 	return ((x2-x1) ** 2 + (y2-y1) ** 2) ** .5
 
-# Calculate angle BCD
-def angleLeft(a,b,c,d):
-	CB = distance(0,b,c,0)
-	CD = distance(c,0,0,d)
-	BD = distance(0,b,0,d)
-	return acos((CB ** 2 + CD ** 2 - BD ** 2) / (2 * CB * CD))
-
-# Calculate angle BAD
-def angleRight(a,b,c,d):
-	AB = distance(a,0,0,b)
-	AD = distance(a,0,0,d)
-	BD = distance(0,b,0,d)
-	return acos((AB ** 2 + AD ** 2 - BD ** 2) / (2 * AB * AD))
-
 # Calculate the area of a quadrilateral
+def area_uncached(a,b,c,d):
+	return (a * b + b * -c + -c * -d + -d * a) / 2
+
 def area(a,b,c,d):
-	AB = distance(a,0,0,b)
-	BC = distance(0,b,c,0)
-	CD = distance(c,0,0,d)
-	DA = distance(0,d,a,0)
-	Left = angleLeft(a,b,c,d)
-	Right = angleRight(a,b,c,d)
-	return round(.5 * BC * CD * sin(Left) + .5 * AB * DA * sin(Right),1)
+	if (b,-c,d,-a) in cacheArea.keys():
+		return cacheArea[(b,-c,d,-a)]
+	if (-c,-d,-a,-b) in cacheArea.keys():
+		return cacheArea[(-c,-d,-a,-b)]
+	if (-d,a,-b,c) in cacheArea.keys():
+		return cacheArea[(-d,a,-b,c)]
+	value = area_uncached(a,b,c,d)
+	cacheArea[(a,b,c,d)] = value
+	return value
 
 # Calculate the number of exterior lattice points of the quadrilateral
-def exterior(a,b,c,d):
+def exterior_uncached(a,b,c,d):
 	LatticePoints_AB = gcd(a,b)
 	LatticePoints_BC = gcd(b,c) * -1
 	LatticePoints_CD = gcd(c,d) * -1
 	LatticePoints_DA = gcd(d,a)
 	return LatticePoints_AB + LatticePoints_BC + LatticePoints_CD + LatticePoints_DA
 
+def exterior(a,b,c,d):
+	if (b,-c,d,-a) in cacheExterior.keys():
+		return cacheExterior[(b,-c,d,-a)]
+	if (-c,-d,-a,-b) in cacheExterior.keys():
+		return cacheExterior[(-c,-d,-a,-b)]
+	if (-d,a,-b,c) in cacheExterior.keys():
+		return cacheExterior[(-d,a,-b,c)]
+	value = exterior_uncached(a,b,c,d)
+	cacheExterior[(a,b,c,d)] = value
+	return value
+	
 count = 0
 for i in array:
 	AREA = area(i[0],i[1],i[2],i[3])
