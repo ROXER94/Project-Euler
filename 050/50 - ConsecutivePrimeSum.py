3d3b3aeef920f464573b7bdf622ae26df76d7025
Robert Oxer <rdo26@cornell.edu>
2016-08-21 23:51:04 -0400

    Add files via upload

--- 050/50 - ConsecutivePrimeSum.py
+++ 050/50 - ConsecutivePrimeSum.py
@@ -1,4 +1,4 @@
-# Calculates the prime below 1000000 that can be written as the sum of the most consecutive primes
+# Calculates the prime below 1,000,000 that can be written as the sum of the most consecutive primes
 
 from itertools import accumulate
 
