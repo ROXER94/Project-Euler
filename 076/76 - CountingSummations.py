fa2c43423e2be2cb7e65d3ff014cc267bedcf263
Robert Oxer <rdo26@cornell.edu>
2016-09-02 16:05:04 -0400

    Add files via upload

---
+++ 076/76 - CountingSummations.py
@@ -0,0 +1,17 @@
+# Calculates the number of different ways 100 can be written as a sum of at least two positive integers
+
+def factors(n):
+	factors_array = []
+	for x in range(1, n//2 + 1):
+		if n % x == 0:
+			factors_array.append(x)
+	factors_array.append(n)
+	return sum(factors_array)
+
+cache = {0:1}
+def P(n):
+	if n not in cache:
+		cache[n] = sum(factors(n-k) * P(k) for k in range(n)) // n
+	return cache[n]
+
+print(P(100)-1)
---
+++ 076/README.md
@@ -0,0 +1,5 @@
+# CountingSummations
+
+From [Project Euler: Problem 76](https://projecteuler.net/problem=76)
+
+Calculates the number of different ways 100 can be written as a sum of at least two positive integers
