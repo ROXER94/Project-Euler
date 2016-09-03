9fc4b7242fe6fcc74664ddae6b3394947a0ecec7
Robert Oxer <rdo26@cornell.edu>
2016-09-03 01:08:45 -0400

    Add files via upload

---
+++ 145/145 - ReversibleNumbers.py
@@ -0,0 +1,16 @@
+# Calculates the number of reversible numbers below 1,000,000,000
+
+def Reverse(n):
+	if str(n)[-1] == "0":
+		return False
+	value = str(n + int(str(n)[::-1]))
+	if "0" in value or "2" in value or "4" in value or "6" in value or "8" in value:
+		return False
+	return True
+
+count = 0
+for i in range(1,100000000):
+	if Reverse(i):
+		count += 1
+
+print(count)
---
+++ 145/README.md
@@ -0,0 +1,5 @@
+# ReversibleNumbers
+
+From [Project Euler: Problem 145](https://projecteuler.net/problem=145)
+
+Calculates the number of reversible numbers below 1,000,000,000
