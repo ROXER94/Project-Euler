488cf177ef787321eba4abaf53bc442be5a81e68
Robert Oxer <rdo26@cornell.edu>
2016-09-04 16:46:06 -0400

    Add files via upload

---
+++ 206/206 - ConcealedSquare.py
@@ -0,0 +1,16 @@
+# Calculates the only positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0
+
+def Concealed(n):
+	n = str(n)
+	return n[0] == "1" and n[2] == "2" and n[4] == "3" and n[6] == "4" and n[8] == "5" and n[10] == "6" and n[12] == "7" and n[14] == "8" and n[16] == "9" and n[18] == "0"
+
+n = (10**9+10**7+10**5+10**3+10**1+20)
+k = True
+while not Concealed(n**2):
+	if k:
+		n += 40
+		k = False
+	else:
+		n += 60
+		k = True
+print(n)
---
+++ 206/README.md
@@ -0,0 +1,5 @@
+# ConcealedSquare
+
+From [Project Euler: Problem 206](https://projecteuler.net/problem=206)
+
+Calculates the only positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0
