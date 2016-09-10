17659d91d8ac56e129432ba8e20031cacd7e5bbd
Robert Oxer <rdo26@cornell.edu>
2016-09-10 01:09:38 -0400

    Add files via upload

---
+++ 079/79 - PasscodeDerivation.py
@@ -0,0 +1,21 @@
+# Calculates the shortest possible secret passcode using the keylog information in the text file
+
+from itertools import permutations
+
+passcodes = sorted([''.join(p) for p in permutations(str(12367890))])
+
+file = open("keylog.txt")
+array = []
+for line in file:
+	i = line.replace("\n","")
+	if i not in array:
+		array.append(i)
+
+for i in passcodes:
+	code = True
+	for j in array:
+		if i.index(j[0]) < i.index(j[1]) < i.index(j[2]):
+			if j == array[-1]:
+				print(i)
+		else:
+			break
---
+++ 079/README.md
@@ -0,0 +1,5 @@
+# PasscodeDerivation
+
+From [Project Euler: Problem 79](https://projecteuler.net/problem=79)
+
+Calculates the shortest possible secret passcode using the keylog information in the text file
