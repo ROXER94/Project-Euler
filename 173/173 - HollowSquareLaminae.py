8a1f70b92fdab31e38c69ab8575a2d056fa6ae9d
Robert Oxer <rdo26@cornell.edu>
2016-09-14 00:33:09 -0400

    Add files via upload

---
+++ 173/173 - HollowSquareLaminae.py
@@ -0,0 +1,18 @@
+# Calculates the number of different square laminae that can be formed using up to 1,000,000 tiles
+
+n = 1000000
+count = 0
+for l in range(3,n//4 + 2):
+	if l%2==0:
+		for s in range(l-2,0,-2):
+			if l**2 - s**2 <= n:
+				count += 1
+			else:
+				break
+	else:
+		for s in range(l-2,0,-2):
+			if l**2 - s**2 <= n:
+				count += 1
+			else:
+				break
+print(count)
---
+++ 173/README.md
@@ -0,0 +1,5 @@
+# HollowSquareLaminae
+
+From [Project Euler: Problem 173](https://projecteuler.net/problem=173)
+
+Calculates the number of different square laminae that can be formed using up to 1,000,000 tiles
