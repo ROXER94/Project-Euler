9855fa012742a2a784177cfc0f0516b0714adebf
Robert Oxer <rdo26@cornell.edu>
2016-09-14 18:51:35 -0400

    Add files via upload

--- 173/173 - HollowSquareLaminae.py
+++ 173/173 - HollowSquareLaminae.py
@@ -3,16 +3,9 @@
 n = 1000000
 count = 0
 for l in range(3,n//4 + 2):
-	if l%2==0:
-		for s in range(l-2,0,-2):
-			if l**2 - s**2 <= n:
-				count += 1
-			else:
-				break
-	else:
-		for s in range(l-2,0,-2):
-			if l**2 - s**2 <= n:
-				count += 1
-			else:
-				break
+	for s in range(l-2,0,-2):
+		if l**2 - s**2 <= n:
+			count += 1
+		else:
+			break
 print(count)
