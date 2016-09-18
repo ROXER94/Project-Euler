f31e5b1bc8f1e0d2fb707cb5f7fd682df82b364d
Robert Oxer <rdo26@cornell.edu>
2016-09-18 15:13:07 -0400

    Add files via upload

--- 086/86 - CuboidRoute.py
+++ 086/86 - CuboidRoute.py
@@ -1,4 +1,4 @@
-# Calculates the minimum size of cuboid routes, up to a maximum size of N x N x N, for which the shortest path has integer length and the number of solutions first exceeds 1,000,000
+# Calculates the minimum size of cuboids, up to a maximum size of N x N x N, for which the shortest path has integer length and the number of solutions first exceeds 1,000,000
 
 from math import sqrt
 
