9378bc3737a369bd98d29783fa4c62440388691c
Robert Oxer <rdo26@cornell.edu>
2016-11-10 23:29:45 -0500

    Add files via upload

--- 108/108 - DiophantineReciprocals1.py
+++ 108/108 - DiophantineReciprocals1.py
@@ -1,6 +1,6 @@
 # Calculates the least value of n for which the number of distinct solutions of the equation x^-1 + y^-1 = n^-1 exceeds 1000
 
-def factors(n):
+def Factors(n):
 	count = 0
 	for x in range(1, int(n**0.5) + 1):
 		if n % x == 0:
@@ -9,7 +9,7 @@ def factors(n):
 
 n = 10
 while True:
-	if factors(n**2) > 2000:
+	if Factors(n**2) > 2000:
 		print(n)
 		break
 	n += 10
