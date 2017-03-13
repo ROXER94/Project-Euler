fd950a91690a65ab72ee1563b8728794f1747e99
Robert Oxer <rdo26@cornell.edu>
2017-03-13 19:09:10 -0400

    Add files via upload

---
+++ 091/91 - RightTrianglesWithIntegerCoordinates.py
@@ -0,0 +1,23 @@
+# Calculates the number of right triangles that can be formed with integer coordinates in a given range and joined at the origin
+
+def right(x1,y1,x2,y2,x3,y3):
+	s1 = distance(x1,y1,x2,y2)
+	s2 = distance(x2,y2,x3,y3)
+	s3 = distance(x1,y1,x3,y3)
+	array = sorted([s1,s2,s3])
+	return -.001 < array[0]**2 + array[1]**2 - array[2]**2 < .001
+
+def distance(x1,y1,x2,y2):
+	return ((x2-x1) ** 2 + (y2-y1) ** 2) ** .5
+
+n = 50
+s = []
+count = 0
+for x1 in range(1,n+1):
+	for y1 in range(0,n+1):
+		for x2 in range(0,n+1):
+			for y2 in range(1,n+1):
+				if right(0,0,x1,y1,x2,y2) and ((x1,y1),(x2,y2)) not in s and ((x2,y2),(x1,y1)) not in s:
+					s.append(((x1,y1),(x2,y2)))
+					count += 1
+print(count-n**2)
---
+++ 091/README.md
@@ -0,0 +1,5 @@
+# RightTrianglesWithIntegerCoordinates
+
+From [Project Euler: Problem 91](https://projecteuler.net/problem=91)
+
+Calculates the number of right triangles that can be formed with integer coordinates in a given range and joined at the origin
