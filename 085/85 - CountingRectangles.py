51fe6c7d7786253c63e4394c1534ce899d49c343
Robert Oxer <rdo26@cornell.edu>
2017-03-15 18:43:18 -0400

    Add files via upload

---
+++ 085/85 - CountingRectangles.py
@@ -0,0 +1,14 @@
+# Calculates the area of the grid contains nearly 2,000,000 rectangles
+
+def rectCount(length,width):
+	return length*width*(length+1)*(width+1)//4
+
+difference = 100
+for l in range(1,1000):
+	for w in range(l,1000):
+		current = rectCount(l,w)
+		if abs(2000000-current) < difference:
+			difference = abs(2000000-current)
+			x = l
+			y = w
+print(x*y)
---
+++ 085/README.md
@@ -0,0 +1,5 @@
+# CountingRectangles
+
+From [Project Euler: Problem 85](https://projecteuler.net/problem=85)
+
+Calculates the area of the grid contains nearly 2,000,000 rectangles
