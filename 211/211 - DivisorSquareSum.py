fb98000eae1eb45d6eb814a1fd01e639bc7edad5
Robert Oxer <rdo26@cornell.edu>
2017-04-05 19:29:28 -0400

    Add files via upload

---
+++ 211/211 - DivisorSquareSum.py
@@ -0,0 +1,16 @@
+# Calculates the sum of all positive numbers n below 64,000,000 such that σ2(n) is a perfect square
+
+from math import sqrt
+
+def isSquare(integer):
+    return int(sqrt(integer) + 0.5) ** 2 == integer
+
+n = 64000000
+sum = 0
+o2 = [1]*n
+for i in range(2,n):
+	for j in range(i,n,i):
+		o2[j] += i**2
+	if isSquare(o2[i]):
+		sum += i
+print(sum+1)
---
+++ 211/README.md
@@ -0,0 +1,5 @@
+# DivisorSquareSum
+
+From [Project Euler: Problem 211](https://projecteuler.net/problem=211)
+
+Calculates the sum of all positive numbers n below 64,000,000 such that σ<sub>2</sub>(n) is a perfect square
