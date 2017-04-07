8f83488645f0656b894f4989fc49c99f5edf3bfd
Robert Oxer <rdo26@cornell.edu>
2017-04-06 21:39:10 -0400

    Add files via upload

---
+++ 277/277 - ModifiedCollatzSequence.py
@@ -0,0 +1,32 @@
+# Calculates the smallest number greater than 10^15 that has a specific modified Collatz sequence
+
+def D(n):
+	return n*3
+
+def U(n):
+	return (3*n-2)/4
+
+def d(n):
+	return (3*n+1)/2
+
+string = "UDDDUdddDDUDDddDdDddDDUDDdUUDd"[::-1]
+n = 1
+while True:
+	original = n
+	for i in string:
+		if i =="D":
+			n = D(n)
+			if n%1!=0:
+				break
+		elif i == "U":
+			n = U(n)
+			if n%1!=0:
+				break
+		elif i == "d":
+			n = d(n)
+			if n%1!=0:
+				break
+	if n%1==0 and n > 10**15:
+		break
+	n = original+69
+print(int(n))
---
+++ 277/README.md
@@ -0,0 +1,5 @@
+# ModifiedCollatzSequence
+
+From [Project Euler: Problem 277](https://projecteuler.net/problem=277)
+
+Calculates the smallest number greater than 10<sup>15</sup> that has a specific modified Collatz sequence
