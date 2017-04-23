408aaaa34770b737c9835f999410da2a9321a88c
Robert Oxer <rdo26@cornell.edu>
2017-04-23 02:33:50 -0400

    Add files via upload

---
+++ 129/129 - RepunitDivisibility.py
@@ -0,0 +1,27 @@
+# Calculates the least value of n for which A(n) first exceeds 1,000,000
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+def powermod(a, b, n):
+	r = 1
+	while b > 0:
+		if b & 1 == 1:
+			r = r * a % n
+		b //= 2
+		a = a * a % n
+	return r
+
+n = 1000013
+while True:
+  if gcd(n,10) == 1:
+    k = 666681
+    while powermod(10,k,n) != 1:
+      k += 1
+    if k > 1000000:
+      print(n,k)
+      break
+  n += 2
---
+++ 129/README.md
@@ -0,0 +1,5 @@
+# RepunitDivisibility
+
+From [Project Euler: Problem 129](https://projecteuler.net/problem=129)
+
+Calculates the least value of n for which A(n) first exceeds 1,000,000
