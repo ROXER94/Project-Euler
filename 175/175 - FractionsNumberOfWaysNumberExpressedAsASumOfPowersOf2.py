c0211709116add870f493b769961b348ef163a46
Robert Oxer <rdo26@cornell.edu>
2017-04-30 14:51:48 -0400

    Add files via upload

---
+++ 175/175 - FractionsNumberOfWaysNumberExpressedAsASumOfPowersOf2.py
@@ -0,0 +1,19 @@
+# Calculates the Shortened Binary Expansion of the smallest n for which f(n)/f(n-1) = 123456789/987654321
+
+from fractions import Fraction
+
+def continuedFraction(r):
+	i = r.numerator // r.denominator
+	f = r - i
+	array.append(i)
+	if f == 0:
+		return
+	continuedFraction(Fraction(1,f))
+
+array = []
+continuedFraction(Fraction(123456789,987654321))
+array = array[1:]
+if len(array) % 2 == 0:
+	array[-1] -= 1
+	array.append(1)
+print(array[::-1])
---
+++ 175/README.md
@@ -0,0 +1,5 @@
+# FractionsNumberOfWaysNumberExpressedAsASumOfPowersOf2
+
+From [Project Euler: Problem 175](https://projecteuler.net/problem=175)
+
+Calculates the Shortened Binary Expansion of the smallest n for which f(n)/f(n-1) = 123456789/987654321
