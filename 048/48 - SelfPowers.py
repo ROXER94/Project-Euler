54aa19599da5eff724e6bb066bf6078a2d8aacbe
Robert Oxer <rdo26@cornell.edu>
2018-03-11 15:55:14 -0400

    Add files via upload

--- 048/48 - SelfPowers.py
+++ 048/48 - SelfPowers.py
@@ -1,6 +1,3 @@
 # Calculates the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000
 
-sequence = 0
-for i in range(1,1000):
-	sequence += i**i
-print(str(sequence)[-10:])
+print(str(sum(i**i for i in range(1,1000)))[-10:])
