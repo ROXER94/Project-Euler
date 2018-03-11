2939426bfd0daacf3b9deb8bbaefa2cb7abe571e
Robert Oxer <rdo26@cornell.edu>
2018-03-11 18:48:04 -0400

    Add files via upload

--- 065/65 - ContinuedFraction.py
+++ 065/65 - ContinuedFraction.py
@@ -2,23 +2,18 @@
 
 from fractions import Fraction
 
-array = [2,1]
-
-k=2
-
-convergent = 100
-
-for i in range(convergent-2):
-	if i%3==0:
-		array.append(k)
-		k += 2
-	else:
-		array.append(1)
-
 def continuedFraction(array):
 	if len(array) != 2:
 		return array[0] + Fraction(1,continuedFraction(array[1:]))
 	else:
 		return array[0] + Fraction(1,array[1])
 
+array = [2,1]
+k=2
+for i in range(98):
+	if i % 3 == 0:
+		array.append(k)
+		k += 2
+	else:
+		array.append(1)
 print(sum(int(i) for i in str(continuedFraction(array).numerator)))
