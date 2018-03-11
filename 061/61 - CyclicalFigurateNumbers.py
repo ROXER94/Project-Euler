6d63aefbf7e4554045f5765df9d0a529935451c1
Robert Oxer <rdo26@cornell.edu>
2018-03-11 18:22:05 -0400

    Add files via upload

--- 061/61 - CyclicalFigurateNumbers.py
+++ 061/61 - CyclicalFigurateNumbers.py
@@ -1,24 +1,25 @@
 # Calculates the sum of the only set of six cyclic 4-digit numbers that contains a triangle, square, pentagonal, hexagonal, heptagonal, and octogonal number
 
+from sys import exit
 from itertools import permutations
 
 def Triangle(n):
-	return n * (n+1) // 2
+	return n*(n+1)//2
 
 def Square(n):
 	return n**2
 
 def Pentagonal(n):
-	return n * (3*n - 1) // 2
+	return n*(3*n-1)//2
 
 def Hexagonal(n):
-	return n * (2*n - 1)
+	return n*(2*n-1)
 
 def Heptagonal(n):
-	return n * (5*n - 3) // 2
+	return n*(5*n-3)//2
 
 def Octogonal(n):
-	return n * (3*n - 2)
+	return n*(3*n-2)
 
 def isCyclic(n,m):
 	return str(n)[2:] == str(m)[:2]
@@ -32,17 +33,18 @@ octogons = [Octogonal(i) for i in range(19,59)]
 
 array = list(permutations([triangles,squares,pentagons,hexagons,heptagons,octogons]))
 
-for list in array[:len(array)//40]:
-	for a in list[0]:
-		for b in list[1]:
+for i in array:
+	for a in i[0]:
+		for b in i[1]:
 			if isCyclic(a,b):
-				for c in list[2]:
+				for c in i[2]:
 					if isCyclic(b,c):
-						for d in list[3]:
+						for d in i[3]:
 							if isCyclic(c,d):
-								for e in list[4]:
+								for e in i[4]:
 									if isCyclic(d,e):
-										for f in list[5]:
+										for f in i[5]:
 											if isCyclic(e,f):
 												if isCyclic(f,a):
 													print(a+b+c+d+e+f)
+													exit()
