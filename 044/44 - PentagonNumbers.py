2a78f9c86340cd901828797917eadcc2304c1a49
Robert Oxer <rdo26@cornell.edu>
2018-03-11 15:37:31 -0400

    Add files via upload

--- 044/44 - PentagonNumbers.py
+++ 044/44 - PentagonNumbers.py
@@ -1,16 +1,17 @@
 # Calculates the minimum difference of two pentagonal numbers whose sum and difference are pentagonal
 
 def pentagonal(n):
-	return int(n * (3*n - 1) / 2)
+	return n*(3*n-1)//2
 
-pentagon = [pentagonal(n) for n in range(1,2500)]
+pentagon = set(pentagonal(n) for n in range(1,2500))
 
 array = []
 for p1 in pentagon:
-	for p2 in pentagon[pentagon.index(p1)+1:]:
+	for p2 in pentagon:
 		if p1 + p2 in pentagon:
 			array.append((p1,p2))
 
 for i in array:
 	if i[1] - i[0] in pentagon:
 		print(i[1]-i[0])
+		break
