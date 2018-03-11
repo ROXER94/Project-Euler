7f387ee8121fdbea40463a2bf194958782c74230
Robert Oxer <rdo26@cornell.edu>
2018-03-11 14:34:20 -0400

    Add files via upload

--- 030/30 - DigitFifthPowers.py
+++ 030/30 - DigitFifthPowers.py
@@ -1,10 +1,3 @@
 # Calculates the sum of all the numbers that can be written as the sum of fifth powers of their digits
 
-sum = 0
-for i in range(2,6*9**5):
-	total = 0
-	for j in str(i):
-		total += int(j)**5
-	if total == i:
-		sum += i
-print(sum)
+print(sum(i for i in range(2,6*9**5) if sum(int(j)**5 for j in str(i)) == i))
