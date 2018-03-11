8ea0c3e6da308283d20fd31ee5fc4ca843b7c8d8
Robert Oxer <rdo26@cornell.edu>
2018-03-11 13:47:51 -0400

    Add files via upload

--- 022/22 - NameScores.py
+++ 022/22 - NameScores.py
@@ -6,11 +6,4 @@ lettertonumber=dict(zip(string.ascii_letters,[ord(c)%32 for c in string.ascii_le
 
 names = sorted(''.join(open("names.txt").read()).replace('"', '').strip().split(','))
 
-totalscore = 0
-
-for i in names:
-	score = 0
-	for j in str(i):
-		score += lettertonumber[j]
-	totalscore += score * (names.index(i) + 1)
-print(totalscore)
+print(sum(sum(lettertonumber[j] for j in str(i)) * (names.index(i)+1) for i in names))
