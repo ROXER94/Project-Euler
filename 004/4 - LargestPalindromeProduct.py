190a4814eaf542217ba069f4b3ef860f4c3b8bb0
Robert Oxer <rdo26@cornell.edu>
2018-03-11 13:01:09 -0400

    Add files via upload

--- 004/4 - LargestPalindromeProduct.py
+++ 004/4 - LargestPalindromeProduct.py
@@ -1,15 +1,13 @@
 # Calculates the largest palindrome made from the product of two 3-digit numbers
 
 # Determines if a number is palindromic
-def palindrome(number):
-    if str(number) == str(number)[::-1]: 
-        return True
-    return False
+def palindrome(n):
+    return str(n) == str(n)[::-1]
 
 # Determines if a number is the product of two 3-digit numbers
 def threedigitprod(n):
 	for i in range(100,999):
-		if n%i==0 and len(str(int(n/i))) == 3:
+		if n%i==0 and len(str(n//i)) == 3:
 			return True
 	return False
 
