e915e08f4b67219d7f7d69203bff370205454071
Robert Oxer <rdo26@cornell.edu>
2018-03-11 15:53:39 -0400

    Add files via upload

--- 045/45 - TriangularPentagonalHexagonal.py
+++ 045/45 - TriangularPentagonalHexagonal.py
@@ -1,23 +1,18 @@
 # Calculates the next Triangle, Pentagonal, and Hexagonal number after 40755
 
 def triangle(n):
-	return int(n * (n+1)/2)
+	return n*(n+1)//2
 
 def pentagonal(n):
-	return int(n * (3 * n - 1)/2)
+	return n*(3*n-1)//2
 # Every pentagonal number is also a triangle number
 
 def hexagonal(n):
-	return n * (2 * n - 1)
+	return n*(2*n-1)
 
-# Array of Pentagonal numbers
-parray = []
-
-# Array of Hexagonal numbers
-harray = []
-
+hexagons = set()
 for i in range(144,50000):
-	harray.append(hexagonal(i))
-	if pentagonal(i) in harray:
+	hexagons.add(hexagonal(i))
+	if pentagonal(i) in hexagons:
 		print(pentagonal(i))
 		break
