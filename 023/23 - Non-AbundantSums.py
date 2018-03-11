96cdbc21bbf4da78af0e9392ad492350d2a28f8a
Robert Oxer <rdo26@cornell.edu>
2018-03-11 14:26:41 -0400

    Add files via upload

--- 023/23 - Non-AbundantSums.py
+++ 023/23 - Non-AbundantSums.py
@@ -1,33 +1,16 @@
 # Calculates the sum of all the positive integers which cannot be written as the sum of two abundant numbers
 
-cache = {}
 def isAbundant(n):
-	factors_array = []
-	for x in range(1, n//2 + 1):
-	    if n % x == 0:
-	        value = int(n/x)
-	        if value in cache:
-	            return cache[value]
-	        factors_array.append(x)
-	if sum(factors_array) > n:
-		cache[n] = True
-		return True
-	return False
+	factors = []
+	for i in range(1,n//2+1):
+		if n%i == 0:
+			factors.append(i)
+	return sum(factors) > n
 
-abundant = []
-for i in range(12,28124):
-	if isAbundant(i):
-		abundant.append(i)
-
-sums = []
-for i in range(len(abundant)):
-	for j in range(i,len(abundant)):
-		value = abundant[i]+abundant[j]
-		sums.append(value)
-sums = list(set(sums))
-
-sum = 0
-for i in range(1,28124):
-	if i not in sums:
-		sum += i
-print(sum)
+abundants = [n for n in range(1,28124) if isAbundant(n)]
+sums = set()
+for i in abundants:
+	for j in abundants:
+		if j >= i:
+			sums.add(i+j)
+print(sum(n for n in range(1,28124) if n not in sums))
