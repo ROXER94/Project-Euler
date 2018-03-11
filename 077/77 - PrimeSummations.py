8f6e6cd1ba0c75214b6cf84cd01b32455cea8630
Robert Oxer <rdo26@cornell.edu>
2018-03-11 19:53:32 -0400

    Add files via upload

--- 077/77 - PrimeSummations.py
+++ 077/77 - PrimeSummations.py
@@ -4,16 +4,14 @@ max = 100
 primes = []
 sieve = [True] * max
 for i in range(2, max):
-    if sieve[i]:
-        primes.append(i)
-        for j in range(i * i, max, i):
-            sieve[j] = False
+	if sieve[i]:
+		primes.append(i)
+		for j in range(i * i, max, i):
+			sieve[j] = False
 
 cache = {}
-i = 1
 for p in primes:
-	cache[i] = p
-	i += 1
+	cache[len(primes)+1] = p
 
 n = 1
 result = 1
@@ -23,6 +21,6 @@ while result <= 5000:
 	ways[0] = 1
 	for i in range(1,len(cache)+1):
 		for j in range(cache[i],n+1):
-			ways[j] = ways [j] + ways [j - cache[i]]
+			ways[j] = ways [j] + ways [j-cache[i]]
 	result = ways[n]
 print(n)
