8fd90e38020bff722e8526a4be4ddf632a5b8b40
Robert Oxer <rdo26@cornell.edu>
2018-03-11 17:50:14 -0400

    Add files via upload

--- 056/56 - PowerfulDigitSum.py
+++ 056/56 - PowerfulDigitSum.py
@@ -1,12 +1,7 @@
 # Calculates the maximum digital sum of natural numbers of the form a^b for a,b < 100
 
 maximum = 0
-
 for a in range(1,100):
 	for b in range(1,100):
-		current = 0
-		for x in str(a**b):
-			current += int(x)
-		if current > maximum:
-			maximum = current
+		maximum = max(maximum,sum(int(i) for i in str(a**b)))
 print(maximum)
