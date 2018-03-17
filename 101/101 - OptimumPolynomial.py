cf3af03fa7b9a6b0d5b2c7311e3f9b26877aae75
Robert Oxer <rdo26@cornell.edu>
2018-03-17 15:56:09 -0400

    Add files via upload

--- 101/101 - OptimumPolynomial.py
+++ 101/101 - OptimumPolynomial.py
@@ -18,16 +18,16 @@ def TransposeMatrix(m):
 def MatrixMinor(m,i,j):
 	return [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
 
-def MatrixDeternminant(m):
+def MatrixDeterminant(m):
 	if len(m) == 2:
 		return m[0][0]*m[1][1]-m[0][1]*m[1][0]
 	determinant = 0
 	for c in range(len(m)):
-		determinant += ((-1)**c)*m[0][c]*MatrixDeternminant(MatrixMinor(m,0,c))
+		determinant += ((-1)**c)*m[0][c]*MatrixDeterminant(MatrixMinor(m,0,c))
 	return determinant
 
 def MatrixInverse(m):
-	determinant = MatrixDeternminant(m)
+	determinant = MatrixDeterminant(m)
 	if len(m) == 2:
 		return [[m[1][1]/determinant, -1*m[0][1]/determinant],[-1*m[1][0]/determinant, m[0][0]/determinant]]
 	cofactors = []
@@ -35,7 +35,7 @@ def MatrixInverse(m):
 		cofactorRow = []
 		for c in range(len(m)):
 			minor = MatrixMinor(m,r,c)
-			cofactorRow.append(((-1)**(r+c)) * MatrixDeternminant(minor))
+			cofactorRow.append(((-1)**(r+c)) * MatrixDeterminant(minor))
 		cofactors.append(cofactorRow)
 	cofactors = TransposeMatrix(cofactors)
 	for r in range(len(cofactors)):
@@ -44,8 +44,7 @@ def MatrixInverse(m):
 	return cofactors
 
 def MatrixMultiplication(a,b):
-	zip_b = zip(*b)
-	zip_b = list(zip_b)
+	zip_b = list(zip(*b))
 	return [[sum(ele_a*ele_b for ele_a, ele_b in zip(row_a, col_b)) for col_b in zip_b] for row_a in a][::-1]
 
 S = 0
