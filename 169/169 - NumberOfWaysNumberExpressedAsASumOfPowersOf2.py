3f86fe2570584dda1f3523219c18eacc32b91364
Robert Oxer <rdo26@cornell.edu>
2018-03-17 18:21:53 -0400

    Add files via upload

--- 169/169 - NumberOfWaysNumberExpressedAsASumOfPowersOf2.py
+++ 169/169 - NumberOfWaysNumberExpressedAsASumOfPowersOf2.py
@@ -1,16 +1,14 @@
 # Calculates the number of ways 10^25 can be expressed as a sum of powers of two using each power no more than twice
 
 def fusc(n):
-	k = n
 	a = 1
 	b = 0
-	while k > 0:
-		if k%2 == 0:
-			k //= 2
+	while n > 0:
+		if n % 2 == 0:
 			a += b
 		else:
-			k = (k-1)//2
 			b += a
+		n //= 2
 	return b
 
 print(fusc(10**25+1))
