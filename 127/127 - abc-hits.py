8c4258d00421c5f2a97dbd26eedff3b9ccb17346
Robert Oxer <rdo26@cornell.edu>
2018-03-17 17:42:23 -0400

    Add files via upload

--- 127/127 - abc-hits.py
+++ 127/127 - abc-hits.py
@@ -1,55 +1,21 @@
 # Calculates the sum of c less than 120,000 for abc-hits 
 
-from fractions import gcd
-from functools import reduce
-from operator import mul
+from math import gcd
 
-def prime_factors(n):
-	if n not in factors_cache:
-		factors_cache[n] = prime_factors_uncached(n)
-	return factors_cache[n]
-
-def prime_factors_uncached(n):
-	i = 2
-	factors = []
-	while i * i <= n:
-		if n % i:
-			i += 1
-		else:
-			n //= i
-			factors.append(i)
-	if n > 1:
-		factors.append(n)
-	return list(set(factors))
-
-rad_cache = {}
-factors_cache = {}
-
-def rad(n):
-	if n not in rad_cache:
-		rad_cache[n] = rad_uncached(n)
-	return rad_cache[n]
-
-def rad_uncached(n):
-	return reduce(mul,prime_factors(n),1)
-
-hits = 0
-n = 120000
-for a in range(1,n//2):
-	if a%2==0:
-		for b in range(a+1,n-a,2):
-			c = a + b
-			radc = rad(c)
-			if radc < c//2:
-				if rad(a)*rad(b)*radc < c:
-					if gcd(a,b) == 1:
-						hits += c
-	else:
-		for b in range(a+1,n-a):
-			c = a + b
-			radc = rad(c)
-			if radc < c//2:
-				if rad(a)*rad(b)*radc < c:
-					if gcd(a,b) == 1:
-						hits += c
-print(hits)
+N = 120000
+rad = [1]*N
+for i in range(2,N):
+	if rad[i] == 1:
+		for j in range(i,N,i):
+			rad[j] *= i
+value = 0
+for c in range(2,N):
+	if rad[c-1] * rad[c] < c:
+		value += c
+	if 6*rad[c] < c:
+		for a in range(2,c//2):
+			if rad[a] * rad[c] < c:
+				if gcd(a,c) == 1:
+					if rad[a] * rad[c-a] * rad[c] < c:
+						value += c
+print(value)
