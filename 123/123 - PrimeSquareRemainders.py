193573ad071987784644e4d4ae0e1ed5630a802e
Robert Oxer <rdo26@cornell.edu>
2018-03-17 16:15:02 -0400

    Add files via upload

--- 123/123 - PrimeSquareRemainders.py
+++ 123/123 - PrimeSquareRemainders.py
@@ -1,25 +1,15 @@
 # Calculates the smallest value of n for which the remainder first exceeds 10^10 in the equation (p(n)-1)^n + (p(n)+1)^n % p(n)^2
 
-def isPrime(n):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-def PrimeSquareRemainder(n):
-	return ((primes[n-1]-1)**(n) + (primes[n-1]+1)**(n)) % primes[n-1]**2
-
-primes = [2] + [p for p in range(3,300000,2) if isPrime(p)]
+max = 300000
+primes = []
+sieve = [True] * max
+for i in range(2, max):
+	if sieve[i]:
+		primes.append(i)
+		for j in range(i * i, max, i):
+			sieve[j] = False
 
 n = 7037
-while PrimeSquareRemainder(n) < 10000000000:
+while 2*(n+1)*primes[n] < 10000000000:
 	n += 2
-print(n)
+print(n+2)
