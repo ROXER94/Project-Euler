15b1c739562cd6e4ab7fad1cf4e7b647597d3ffb
Robert Oxer <rdo26@cornell.edu>
2018-03-17 18:18:25 -0400

    Add files via upload

--- 148/148 - ExploringPascal'sTriangle.py
+++ 148/148 - ExploringPascal'sTriangle.py
@@ -1,34 +1,21 @@
 # Calculates the number of entries which are not divisible by 7 in the first 1,000,000,000 rows of Pascal's triangle
 
-def add(n):
-	result = 1
-	for i in str(n):
-		result *= int(i)+1
-	return result
-
-def triangle(n):
-    return n * (n+1) / 2
-
 def Base7(n):
-    if n == 0:
-        return [0]
-    digits = []
-    while n:
-        digits.append(int(n % 7))
-        n /= 7
-    base7 = ""
-    for i in digits[::-1]:
-    	base7 += str(i)
-    return int(base7.strip("0"))
+	if n == 0:
+		return 0
+	digits = []
+	while n:
+		digits.append(int(n % 7))
+		n /= 7
+	return int(''.join(str(i) for i in digits[::-1]))
 
-n = 1000000000
-n7 = Base7(n)
+def Pascal(n,b = False):
+	if (n < 7): return n * (n+1) // 2
+	b7 = n
+	if b:
+		b7 = Base7(n)
+	n1 = int(str(b7)[0])
+	n2 = int(str(b7)[1:])
+	return n1 * (n1+1) // 2 * 28**(len(str(b7))-1) + (n1+1) * Pascal(n2)
 
-sum = 0
-for i in range(0,11):
-    if len(str(n7)[:i]) == 0:
-        value = 1
-    else:
-        value = add(int(str(n7)[:i]))
-    sum += (value) * triangle(int(str(n7)[i])) * 28**(len(str(n7))-i-1)
-print(int(sum))
+print(Pascal(1000000000,True))
