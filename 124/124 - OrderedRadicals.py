9a8856c280369d845e79970e4b7679f106c877c6
Robert Oxer <rdo26@cornell.edu>
2018-03-17 16:21:21 -0400

    Add files via upload

--- 124/124 - OrderedRadicals.py
+++ 124/124 - OrderedRadicals.py
@@ -1,24 +1,14 @@
 # Calculates the value of n in the 10,000th row of the sorted column of rad(n) for 1 ≤ n ≤ 100,000
 # The radical of n, rad(n), is the product of the distinct prime factors of n
 
-def radical(n):
-    i = 2
-    factors = []
-    while i * i <= n:
-        if n % i:
-            i += 1
-        else:
-            n //= i
-            factors.append(i)
-    if n > 1:
-        factors.append(n)
-    product = 1
-    for i in list(set(factors)):
-    	product *= i
-    return product
-
+N = 100000
+rad = [1]*N
+for i in range(2,N):
+	if rad[i] == 1:
+		for j in range(i,N,i):
+			rad[j] *= i
 array = []
-k = 10000
-for i in range(1,100001):
-	array.append((radical(i),i))
-print(sorted(array)[k-1][1])
+for i in range(N):
+	array.append((rad[i],i))
+array = sorted(array)
+print(array[9999][1])
