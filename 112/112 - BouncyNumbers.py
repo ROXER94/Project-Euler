22c72f0592e6b22474ebc06470446877dd0dd19a
Robert Oxer <rdo26@cornell.edu>
2018-03-17 16:05:38 -0400

    Add files via upload

--- 112/112 - BouncyNumbers.py
+++ 112/112 - BouncyNumbers.py
@@ -7,8 +7,7 @@ def isIncreasing(number):
 		if int(number[i]) <= int(number[i+1]):
 			continue
 		else:
-			increasing = False
-			break
+			return False
 	return increasing
 
 def isDecreasing(number):
@@ -18,19 +17,16 @@ def isDecreasing(number):
 		if int(number[i]) >= int(number[i+1]):
 			continue
 		else:
-			decreasing = False
-			break
+			return False
 	return decreasing
 
 def isBouncy(number):
-    if number < 100:
-        return False
     return not isIncreasing(number) and not isDecreasing(number)
 
 count = 525
 Range = 1000
 while count/Range != .99:
-	if isBouncy(Range+1):
-		count += 1
 	Range += 1
+	if isBouncy(Range):
+		count += 1
 print(Range)
