9e353cd2b91895ac6d2a9b9e118673e4da20eccd
Robert Oxer <rdo26@cornell.edu>
2018-03-17 16:12:05 -0400

    Add files via upload

--- 120/120 - SquareRemainders.py
+++ 120/120 - SquareRemainders.py
@@ -1,11 +1,3 @@
 # Calculates the sum of the maximum remainders of ((a−1)^n + (a+1)^n) / a^2 for 3 ≤ a ≤ 1000
 
-sum = 0
-for a in range(3,1001):
-	max = 0
-	for n in range(1,1500):
-		current = ((a-1)**n + (a+1)**n) % a**2
-		if current > max:
-			max = current
-	sum += max
-print(sum)
+print(sum((i + i % 2 - 2) * i for i in range(3,1001)))
