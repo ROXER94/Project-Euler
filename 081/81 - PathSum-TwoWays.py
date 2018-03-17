d666dab85d7c73844ab34f24ba506660d1a05d18
Robert Oxer <rdo26@cornell.edu>
2018-03-17 15:07:43 -0400

    Add files via upload

--- 081/81 - PathSum-TwoWays.py
+++ 081/81 - PathSum-TwoWays.py
@@ -2,39 +2,13 @@
 
 matrix = []
 file = open("matrix.txt", "r")
-for line in file:
+for line in file.split('\n'):
 	matrix.append([int(i) for i in line.split(",")])
-
-distinct = []
-for i in matrix:
-	for j in i:
-		distinct.append(j)
-distinct = list(set(distinct))
-
-a = 1
-for i in matrix:
-	for j in i:
-		if j in distinct:
-			i[i.index(j)] += .000001 * a
-			a += 1
-
-array = [[] for i in range(2*len(matrix) - 1)]
-
-for x in matrix:
-	for y in x:
-		sum = matrix.index(x) + x.index(y)
-		array[sum].append(y)
-matrix = array
-
-for i in range(len(matrix)):
-	if i >= int(len(matrix)/2):
-		length = len(matrix[i])
-		missing = i + 1 - length
-		for j in range(int(missing/2)):
-			matrix[i] = [10000] + matrix[i]
-			matrix[i].append(10000)
-
-t = matrix[-1]
-for i in range(len(matrix) - 2, -1, -1):
-    t = [min(matrix[i][j] + t[j], matrix[i][j] + t[j+1]) for j in range(len(matrix[i]))]
-print(int(t[0]))
+for i in range(1,80):
+	matrix[0][i] += matrix[0][i-1]
+for j in range(1,80):
+	matrix[j][0] += matrix[j-1][0]
+for i in range(1,80):
+	for j in range(1,80):
+		matrix[i][j] += min(matrix[i][j-1],matrix[i-1][j])
+print(matrix[-1][-1])
