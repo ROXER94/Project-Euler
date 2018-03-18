84e97d02018e81de2ab24f7f642ccedbdb179dbd
Robert Oxer <rdo26@cornell.edu>
2018-03-17 20:35:22 -0400

    Add files via upload

--- 357/357 - PrimeGeneratingIntegers.py
+++ 357/357 - PrimeGeneratingIntegers.py
@@ -1,25 +1,22 @@
 # Calculates the sum of all positive integers n below 100,000,000 such that the sum of each pair of factors of n is prime
 
 max = 100000000
-primes = {}
+P = {}
 sieve = [True] * max
 for i in range(2, max):
 	if sieve[i]:
-		primes[i] = True
+		P[i] = True
 		for j in range(i * i, max, i):
 			sieve[j] = False
-	else:
-		primes[i] = False
+			P[j] = False
 
-def Factors(n):
-	valid = True
+def isPrimeGeneratingInteger(n):
 	for d in range(1, int(n**.5) + 1):
 		if n % d == 0:
-			if primes[d + n//d]:
+			if P[d + n//d]:
 				continue
 			else:
-				valid = False
-				break
-	return valid
+				return False
+	return True
 
-print(sum([n for n in range(2,max+1,4) if primes[n+1] and Factors(n)]) + 1)
+print(sum(n for n in range(2,max+1,4) if P[n+1] and isPrimeGeneratingInteger(n)) + 1)
