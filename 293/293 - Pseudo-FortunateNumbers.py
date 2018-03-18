885117e1ba529961d2bd38856fcaee16decfe4a3
Robert Oxer <rdo26@cornell.edu>
2018-03-17 20:01:32 -0400

    Add files via upload

--- 293/293 - Pseudo-FortunateNumbers.py
+++ 293/293 - Pseudo-FortunateNumbers.py
@@ -1,80 +1,44 @@
 # Calculates the sum of all distinct pseudo-Fortunate numbers for admissible numbers less than 10^9
 
-from math import log
+def isPrime(n):
+	if n <= 1:
+		return False
+	if n <= 3:
+		return True
+	if n % 2 == 0 or n % 3 == 0:
+		return False
+	i = 5
+	while i * i <= n:
+		if n % i == 0 or n % (i + 2) == 0:
+			return False
+		i += 6
+	return True
 
-max = 10**9
-def primes(n):
-	P = {2:True}
-	sieve = [True] * n
-	for i in range(3,int(n**0.5)+1,2):
-		if sieve[i]:
-			sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
-	for i in range(3,n,2):
-		if sieve[i]:
-			P[i] = True
-	return P
+admissible = []
+def Admissable(index,product,N=1000000000):
+	if (index == len(primes)):
+		return
+	while (product < N):
+		product *= primes[index]
+		if (product < N):
+			admissible.append(product)
+		Admissable(index+1,product)
 
-P = primes(max)
-n = max
-array = []
-for a in range(int(log(n,2))+1):
-	for b in range(int(log(n//2,3))+1):
-		if 2**a*3**b < n:
-			for c in range(int(log(n//2,5))+1):
-				if 2**a*3**b*5**c < n:
-					for d in range(int(log(n//2,7))+1):
-						if 2**a*3**b*5**c*7**d < n:
-							for e in range(int(log(n//2,11))+1):
-								if 2**a*3**b*5**c*7**d*11**e < n:
-									for f in range(int(log(n//2,13))+1):
-										if 2**a*3**b*5**c*7**d*11**e*13**f < n:
-											for g in range(int(log(n//2,17))+1):
-												if 2**a*3**b*5**c*7**d*11**e*13**f*17**g < n:
-													for h in range(int(log(n//2,19))+1):
-														if 2**a*3**b*5**c*7**d*11**e*13**f*17**g*19**h < n:
-															for i in range(int(log(n//2,23))+1):
-																if 2**a*3**b*5**c*7**d*11**e*13**f*17**g*19**h*23**i < n:
-																	if a > 0 and b == c == d == e == f == g == h == i == 0:
-																		value = 2**a * 3**b * 5**c * 7**d * 11**e * 13**f * 17**g * 19**h * 23**i
-																		if value < n:
-																			array.append(value)
-																	if a > 0 and b > 0 and c == d == e == f == g == h == i == 0:
-																		value = 2**a * 3**b * 5**c * 7**d * 11**e * 13**f * 17**g * 19**h * 23**i
-																		if value < n:
-																			array.append(value)
-																	if a > 0 and b > 0 and c > 0 and d == e == f == g == h == i == 0:
-																		value = 2**a * 3**b * 5**c * 7**d * 11**e * 13**f * 17**g * 19**h * 23**i
-																		if value < n:
-																			array.append(value)
-																	if a > 0 and b > 0 and c > 0 and d > 0 and e == f == g == h == i == 0:
-																		value = 2**a * 3**b * 5**c * 7**d * 11**e * 13**f * 17**g * 19**h * 23**i
-																		if value < n:
-																			array.append(value)
-																	if a > 0 and b > 0 and c > 0 and d > 0 and e > 0 and f == g == h == i == 0:
-																		value = 2**a * 3**b * 5**c * 7**d * 11**e * 13**f * 17**g * 19**h * 23**i
-																		if value < n:
-																			array.append(value)
-																	if a > 0 and b > 0 and c > 0 and d > 0 and e > 0 and f > 0 and g == h == i == 0:
-																		value = 2**a * 3**b * 5**c * 7**d * 11**e * 13**f * 17**g * 19**h * 23**i
-																		if value < n:
-																			array.append(value)
-																	if a > 0 and b > 0 and c > 0 and d > 0 and e > 0 and f > 0 and g > 0 and h == i == 0:
-																		value = 2**a * 3**b * 5**c * 7**d * 11**e * 13**f * 17**g * 19**h * 23**i
-																		if value < n:
-																			array.append(value)
-																	if a > 0 and b > 0 and c > 0 and d > 0 and e > 0 and f > 0 and g > 0 and h > 0:
-																		value = 2**a * 3**b * 5**c * 7**d * 11**e * 13**f * 17**g * 19**h * 23**i
-																		if value < n:
-																			array.append(value)
+max = 24
+primes = []
+sieve = [True] * max
+for i in range(2, max):
+	if sieve[i]:
+		primes.append(i)
+		for j in range(i * i, max, i):
+			sieve[j] = False
 S = set()
-for N in array:
-	M = 3
-	test = False
-	while not test:
-		try:
-			if P[N+M]:
-				test = True
-		except:
-			M += 2
-	S.add(M)
+Admissable(0,1)
+for i in admissible:
+	j = 3
+	while True:
+		if isPrime(i+j):
+			break
+		j += 2
+	S.add(j)
 print(sum(S))
