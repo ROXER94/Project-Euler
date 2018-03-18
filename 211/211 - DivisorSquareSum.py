64dc4da46bba9c75679275ed275d56afbde6c9f1
Robert Oxer <rdo26@cornell.edu>
2018-03-18 12:43:00 -0400

    Add files via upload

--- 211/211 - DivisorSquareSum.py
+++ 211/211 - DivisorSquareSum.py
@@ -5,12 +5,12 @@ from math import sqrt
 def isSquare(n):
     return int(sqrt(n)+.5)**2 == n
 
+Sum = 0
 n = 64000000
-sum = 0
 o2 = [1]*n
 for i in range(2,n):
 	for j in range(i,n,i):
 		o2[j] += i**2
 	if isSquare(o2[i]):
-		sum += i
-print(sum+1)
+		Sum += i
+print(Sum+1)
