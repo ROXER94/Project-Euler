ef6b371f67c3463e12a0fc7a1b81cdfe2a1621f7
Robert Oxer <rdo26@cornell.edu>
2018-03-18 14:26:05 -0400

    Add files via upload

--- 351/351 - HexagonalOrchards.py
+++ 351/351 - HexagonalOrchards.py
@@ -3,22 +3,21 @@
 import operator as op
 from functools import reduce
 
-def totient_gen(limit):
-	phi = (limit+1)*[0]
+def totients(n):
+	phi = [0]*(n+1)
 	phi[1] = 1
-	yield 1
-	for n in range(2,limit):
-		if phi[n] == 0:
-			phi[n] = n - 1
-			for j in range(2,int(limit/n)):
+	for i in range(1,n+1):
+		if phi[i] == 0:
+			phi[i] = i - 1
+			for j in range(2,n//i+1):
 				if phi[j] != 0:
 					q = j
-					f = n - 1
-					while q % n == 0:
-						f *= n
-						q //= n
-					phi[n * j] = f * phi[q]
-		yield phi[n]
+					f = i - 1
+					while q % i == 0:
+						f *= i
+						q //= i
+					phi[i*j] = f * phi[q]
+		yield phi[i]
 
 def ncr(n, r):
 	r = min(r, n-r)
@@ -28,6 +27,6 @@ def ncr(n, r):
 	return numerator//denominator     
 
 def H(n):
-	return 6 * (ncr(n+1,2) - sum(list(totient_gen(int(1.5*n)))[:n]))
+	return 6 * (ncr(n+1,2) - sum(list(totients(n))))
 
 print(H(100000000))
