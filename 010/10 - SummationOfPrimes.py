f9a01cb18ae9a999dd3799e90948c80b31670785
Robert Oxer <rdo26@cornell.edu>
2018-03-18 13:05:44 -0400

    Add files via upload

--- 010/10 - SummationOfPrimes.py
+++ 010/10 - SummationOfPrimes.py
@@ -1,11 +1,12 @@
 # Calculates the sum of all primes below 2,000,000
 
-def isPrime(n):
-    if not n & 1: 
-        return False
-    for x in range(3, int(n**0.5) + 1, 2):
-        if n % x == 0:
-            return False
-    return True
+max = 2000000
+primes = []
+sieve = [True] * max
+for i in range(2, max):
+	if sieve[i]:
+		primes.append(i)
+		for j in range(i * i, max, i):
+			sieve[j] = False
 
-print(sum([n for n in range(3,2000000,2) if isPrime(n)])+2)
+print(sum(primes))
