e970e22c64988c2b497626ba8a957f70cd042efb
Robert Oxer <rdo26@cornell.edu>
2018-03-18 13:09:09 -0400

    Add files via upload

--- 041/41 - PandigitalPrime.py
+++ 041/41 - PandigitalPrime.py
@@ -3,16 +3,18 @@
 from itertools import permutations
 
 def isPrime(n):
-    if n < 2:
-        return False
-    if n == 2: 
-        return True    
-    if not n & 1: 
-        return False
-    for x in range(3, int(n**0.5) + 1, 2):
-        if n % x == 0:
-            return False
-    return True
+	if n <= 1:
+		return False
+	if n <= 3:
+		return True
+	if n % 2 == 0 or n % 3 == 0:
+		return False
+	i = 5
+	while i * i <= n:
+		if n % i == 0 or n % (i + 2) == 0:
+			return False
+		i += 6
+	return True
 
 perms = [''.join(p) for p in permutations(str(7654321))]
 for i in perms:
