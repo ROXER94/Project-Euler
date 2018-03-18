d6904e9c9a48c097dc32fe97f5dce2db4a578ac5
Robert Oxer <rdo26@cornell.edu>
2018-03-18 13:08:00 -0400

    Add files via upload

--- 026/26 - ReciprocalCycles.py
+++ 026/26 - ReciprocalCycles.py
@@ -1,22 +1,15 @@
 # Calculates the number below 1000 which contains the longest recurring cycle in its decimal fraction part
 
-def isPrime(n):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-	
-primes = [p for p in range(3,1000,2) if isPrime(p)][::-1] + [2]
+max = 1000
+primes = []
+sieve = [True] * max
+for i in range(2, max):
+	if sieve[i]:
+		primes.append(i)
+		for j in range(i * i, max, i):
+			sieve[j] = False
 
-for p in primes:
+for p in primes[::-1]:
 	t = 1
 	while (10**t)%p != 1:
 		t += 1
