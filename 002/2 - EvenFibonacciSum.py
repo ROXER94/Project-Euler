0d2b1acd5ed01c58261719cde96e16d291a4c0eb
Robert Oxer <rdo26@cornell.edu>
2018-03-18 16:45:31 -0400

    Add files via upload

--- 002/2 - EvenFibonacciSum.py
+++ 002/2 - EvenFibonacciSum.py
@@ -3,14 +3,12 @@
 cache_fib = {}
 
 def fib(n):
-    if n not in cache_fib.keys():
-        cache_fib[n] = fibonacci_uncached(n)
-    return cache_fib[n]
+	if n not in cache_fib.keys():
+		cache_fib[n] = fibonacci_uncached(n)
+	return cache_fib[n]
 
 def fibonacci_uncached(n):
-    if n < 2:
-        return n
-    else:
-        return fib(n-1) + fib(n-2)
+	if n < 2: return n
+	else: return fib(n-1) + fib(n-2)
 
-print(sum(fib(x) for x in range(1,10000) if fib(x) < 4000000 and fib(x)%2==0))
+print(sum(fib(x) for x in range(1,10000) if fib(x) < 4000000 and fib(x) % 2 == 0))
