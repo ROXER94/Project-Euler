89bef838671e19137f66d0026c6cbd2f34548ec0
Robert Oxer <rdo26@cornell.edu>
2018-03-18 12:55:11 -0400

    Add files via upload

--- 035/35 - CircularPrimes.py
+++ 035/35 - CircularPrimes.py
@@ -1,16 +1,18 @@
 # Calculates the number of Circular Primes below 1,000,000
 
 def isPrime(n):
-    if n < 2:
-        return False
-    if n == 2: 
-        return True    
-    if not n & 1: 
-        return False
-    for x in range(3, int(n**0.5) + 1, 2):
-        if n % x == 0:
-            return False
-    return True
+	if n <= 1:
+		return False
+	if n <= 3:
+		return True
+	if n % 2 == 0 or n % 3 == 0:
+		return False
+	i = 5
+	while i * i <= n:
+		if n % i == 0 or n % (i + 2) == 0:
+			return False
+		i += 6
+	return True
 	
 def isCircularPrime(n):
 	n = str(n)
