178e18ef6a7110ea5b01c6b64ad268b43e4a5424
Robert Oxer <rdo26@cornell.edu>
2018-03-18 12:47:47 -0400

    Add files via upload

--- 218/218 - PerfectRightTriangles.py
+++ 218/218 - PerfectRightTriangles.py
@@ -5,6 +5,9 @@ sys.setrecursionlimit(10000)
 
 from math import sqrt
 
+def isSquare(n):
+    return int(sqrt(n)+.5)**2 == n
+
 def mm(A,B):
 	array = []
 	for i in range(3):
@@ -14,9 +17,6 @@ def mm(A,B):
 		array.append(temp)
 	return array
 
-def isSquare(n):
-    return int(sqrt(n)+.5)**2 == n
-
 A = [[1,-2,2],[2,-1,2],[2,-2,3]]
 B = [[1,2,2],[2,1,2],[2,2,3]]
 C = [[-1,2,2],[-2,1,2],[-2,2,3]]
@@ -24,14 +24,13 @@ C = [[-1,2,2],[-2,1,2],[-2,2,3]]
 def primitive(array):
 	if array[2] > limit:
 		return 0
-	else:
-		a = mm(A,array)
-		b = mm(B,array)
-		c = mm(C,array)
-		area = array[0] * array[1] // 2
-		if isSquare(array[2]) and area % 84 != 0:
-			return 1 + primitive(a) + primitive(b) + primitive(c)
-		return primitive(a) + primitive(b) + primitive(c)
+	a = mm(A,array)
+	b = mm(B,array)
+	c = mm(C,array)
+	area = array[0] * array[1] // 2
+	if isSquare(array[2]) and area % 84 != 0:
+		return 1 + primitive(a) + primitive(b) + primitive(c)
+	return primitive(a) + primitive(b) + primitive(c)
 
-limit = 10**16**(1/2)
+limit = (10**16)**(1/3)
 print(primitive([3,4,5]))
