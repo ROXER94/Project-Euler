670b7ac5645ff77931bcd6169a5c730dd721a7de
Robert Oxer <rdo26@cornell.edu>
2018-03-18 13:15:40 -0400

    Add files via upload

--- 087/87 - PrimePowerTriples.py
+++ 087/87 - PrimePowerTriples.py
@@ -1,23 +1,16 @@
 # Calculates the number of numbers below 50,000,000 that can be expressed as the sum of a prime square, prime cube, and prime fourth power
 
-def isPrime(n):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-primes = [2] + [i for i in range(3,7070,2) if isPrime(i)]
+max = 7070
+primes = []
+sieve = [True] * max
+for i in range(2, max):
+	if sieve[i]:
+		primes.append(i)
+		for j in range(i * i, max, i):
+			sieve[j] = False
 
 Range = 50000000
-array = []
+set = set()
 for square in range(2,7072):
 	if square in primes:
 		for cube in range(2,369):
@@ -27,5 +20,5 @@ for square in range(2,7072):
 						if fourth in primes:
 							number = int(square**2 + cube**3 + fourth**4)
 							if number < Range:
-								array.append(number)
-print(len(list(set(array))))
+								set.add(number)
+print(len(set))
