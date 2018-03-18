9b24e5626459388bb5e27249e9b8001979f3b94b
Robert Oxer <rdo26@cornell.edu>
2018-03-18 16:40:43 -0400

    Add files via upload

--- 034/34 - Factorion.py
+++ 034/34 - Factorion.py
@@ -7,17 +7,9 @@ def fact(n):
 	if n not in cache_fact.keys():
 		cache_fact[n] = factorial_uncached(n)
 	return cache_fact[n]
-	
+
 def factorial_uncached(n):
-    if n == 0 : return 1
-    else :
-        return n*fact(n-1)
-		
-array = []
-for i in range(10,50000):
-	Sum = 0
-	for j in str(i):
-		Sum += fact(int(j))
-	if Sum == i:
-		array.append(i)
-print(sum(array))
+	if n == 0: return 1
+	else: return n*fact(n-1)
+
+print(sum(i for i in range(10,50000) if sum(fact(int(j)) for j in str(i)) == i))
