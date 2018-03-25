99b536e4bd5db094effb29e4528d8f7414fa7e6b
Robert Oxer <rdo26@cornell.edu>
2018-03-25 12:22:11 -0400

    Add files via upload

--- 191/191 - PrizeStrings.py
+++ 191/191 - PrizeStrings.py
@@ -2,34 +2,34 @@
 
 cache = {}
 
-def PrizeString(string,numD,numA,D):
+def PrizeString(string,numL,numA,D):
 	if D == 30:
 		return 1
-	if numD == 1:
+	if numL == 1:
 		if numA == 2:
-			if (numD,numA,D) not in cache:
-				cache[(numD,numA,D)] = PrizeString(string+"O",numD,0,D+1)
-			return cache[(numD,numA,D)]
+			if (numL,numA,D) not in cache:
+				cache[(numL,numA,D)] = PrizeString(string+"O",numL,0,D+1)
+			return cache[(numL,numA,D)]
 		else:
-			if (numD,0,D+1) not in cache:
-				cache[numD,0,D+1] = PrizeString(string+"O",numD,0,D+1)
-			if (numD,numA+1,D+1) not in cache:
-				cache[numD,numA+1,D+1] = PrizeString(string+"A",numD,numA+1,D+1)
-			return cache[numD,0,D+1] + cache[numD,numA+1,D+1]
+			if (numL,0,D+1) not in cache:
+				cache[numL,0,D+1] = PrizeString(string+"O",numL,0,D+1)
+			if (numL,numA+1,D+1) not in cache:
+				cache[numL,numA+1,D+1] = PrizeString(string+"A",numL,numA+1,D+1)
+			return cache[numL,0,D+1] + cache[numL,numA+1,D+1]
 	else:
 		if numA == 2:
-			if (numD,0,D+1) not in cache:
-				cache[numD,0,D+1] = PrizeString(string+"O",numD,0,D+1)
-			if (numD+1,0,D+1) not in cache:
-				cache[numD+1,0,D+1] = PrizeString(string+"A",numD+1,0,D+1)
-			return cache[numD,0,D+1] + cache[numD+1,0,D+1]
+			if (numL,0,D+1) not in cache:
+				cache[numL,0,D+1] = PrizeString(string+"O",numL,0,D+1)
+			if (numL+1,0,D+1) not in cache:
+				cache[numL+1,0,D+1] = PrizeString(string+"A",numL+1,0,D+1)
+			return cache[numL,0,D+1] + cache[numL+1,0,D+1]
 		else:
-			if (numD,0,D+1) not in cache:
-				cache[numD,0,D+1] = PrizeString(string+"O",numD,0,D+1)
-			if (numD,numA+1,D+1) not in cache:
-				cache[numD,numA+1,D+1] = PrizeString(string+"O",numD,numA+1,D+1)
-			if (numD+1,0,D+1) not in cache:
-				cache[numD+1,0,D+1] = PrizeString(string+"O",numD+1,0,D+1)
-			return cache[numD,0,D+1] + cache[numD,numA+1,D+1] + cache[numD+1,0,D+1]
+			if (numL,0,D+1) not in cache:
+				cache[numL,0,D+1] = PrizeString(string+"O",numL,0,D+1)
+			if (numL,numA+1,D+1) not in cache:
+				cache[numL,numA+1,D+1] = PrizeString(string+"O",numL,numA+1,D+1)
+			if (numL+1,0,D+1) not in cache:
+				cache[numL+1,0,D+1] = PrizeString(string+"O",numL+1,0,D+1)
+			return cache[numL,0,D+1] + cache[numL,numA+1,D+1] + cache[numL+1,0,D+1]
 
 print(PrizeString("",0,0,0))
