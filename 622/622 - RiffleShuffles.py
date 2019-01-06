4bdd2274f65492ee5cbb02f96012d4ba75c24603
Robert Oxer <rdo26@cornell.edu>
2019-01-06 16:12:46 -0500

    Add files via upload

---
+++ 622/622 - RiffleShuffles.py
@@ -0,0 +1,25 @@
+# Calculates the sum of all values of n that satisfy s(n)=60
+
+from math import gcd,sqrt
+
+def Factors(n):
+	factors = set()
+	for i in range(1,int(sqrt(n))+1):
+		if n % i == 0:
+			factors.add(i)
+			factors.add(n//i)
+	return sorted(list(factors))
+
+def MultiplicativeOrder(a,n):
+	if gcd(a,n) != 1:
+		return -1
+	r = 1
+	k = 1
+	while k < n:
+		r = r*a % n
+		if r == 1:
+			return k
+		k += 1
+	return -1
+
+print(sum(i+1 for i in Factors(2**60-1) if MultiplicativeOrder(2,i) == 60))
---
+++ 622/README.md
@@ -0,0 +1,5 @@
+# RiffleShuffles
+
+From [Project Euler: Problem 622](https://projecteuler.net/problem=622)
+
+Calculates the sum of all values of n that satisfy s(n)=60
