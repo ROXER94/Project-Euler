5390039e15bc4bcb38a4394bc7b4fdb32b072244
Robert Oxer <rdo26@cornell.edu>
2019-04-16 19:42:52 -0400

    Add files via upload

--- 112/112 - BouncyNumbers.py
+++ 112/112 - BouncyNumbers.py
@@ -1,24 +1,22 @@
 # Calculates the least number for which the proportion of bouncy numbers is exactly 99%
 
 def isIncreasing(number):
-	increasing = True
 	number = str(number)
 	for i in range(len(number)-1):
 		if int(number[i]) <= int(number[i+1]):
 			continue
 		else:
 			return False
-	return increasing
+	return True
 
 def isDecreasing(number):
-	decreasing = True
 	number = str(number)
 	for i in range(len(number)-1):
 		if int(number[i]) >= int(number[i+1]):
 			continue
 		else:
 			return False
-	return decreasing
+	return True
 
 def isBouncy(number):
     return not isIncreasing(number) and not isDecreasing(number)
